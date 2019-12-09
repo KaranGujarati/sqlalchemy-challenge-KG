ebf3223845bcb75fced2616099dbc0085653f600
Karan Gujarati <karan28gujarati@gmail.com>
2019-12-09 13:32:45 -0500

    update on app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
-import pandas as pd
 import datetime as dt
+import pandas as pd
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -11,7 +12,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False})
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -52,18 +53,102 @@ last12mnths = dt.datetime.strftime(last12mnths, '%Y-%m-%d')
 
 @app.route("/")
 def homepage():
-    return (f"Welcome to Surf's Up!: Hawai'i Climate API<br/>"
-            f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<br/>"
+    return (f"Welcome to Surf's Up!: Hawaii's Climate API<br/>"
+            f"-------------------------------------------------<br/>"
             f"Available Routes:<br/>"
-            f"/api/v1.0/stations ~~~~~ a list of all weather observation stations<br/>"
-            f"/api/v1.0/precipitaton ~~ the latest year of preceipitation data<br/>"
-            f"/api/v1.0/temperature ~~ the latest year of temperature data<br/>"
-            f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<br/>"
-            f"~~~ datesearch (yyyy-mm-dd)<br/>"
-            f"/api/v1.0/datesearch/2015-05-30  ~~~~~~~~~~~ low, high, and average temp for date given and each date after<br/>"
-            f"/api/v1.0/datesearch/2015-05-30/2016-01-30 ~~ low, high, and average temp for date given and each date up to and including end date<br/>"
-            f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<br/>"
-            f"~ data available from 2010-01-01 to 2017-08-23 ~<br/>"
-            f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            f"<br/>"
+            f"/api/v1.0/stations <br/>"
+            f"/api/v1.0/precipitaton <br/>"
+            f"/api/v1.0/temperature <br/>"
+            f"-------------------------------------------------<br/>"
+            f"example1: /api/v1.0/datesearch/2017-01-01 <br/>"
+            f"example2: /api/v1.0/datesearch/2016-01-01/2017-01-01 <br/>"
+            f"-------------------------------------------------<br/>"
+            f"data available from 2010-01-01 to 2017-08-23 <br/>")
 
 
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.name).all()
+    all_stations = list(np.ravel(results))
+    return jsonify(all_stations)
+
+
+@app.route("/api/v1.0/precipitaton")
+def precipitation():
+    
+    results = (session.query(Measurement.date, Measurement.prcp, Measurement.station)
+                      .filter(Measurement.date > last12mnths)
+                      .order_by(Measurement.date)
+                      .all())
+    
+    precipData = []
+    for result in results:
+        precipDict = {result.date: result.prcp, "Station": result.station}
+        precipData.append(precipDict)
+
+    return jsonify(precipData)
+
+
+
+@app.route("/api/v1.0/temperature")
+def temperature():
+
+    results = (session.query(Measurement.date, Measurement.tobs, Measurement.station)
+                      .filter(Measurement.date > last12mnths)
+                      .order_by(Measurement.date)
+                      .all())
+
+    tempData = []
+    for result in results:
+        tempDict = {result.date: result.tobs, "Station": result.station}
+        tempData.append(tempDict)
+
+    return jsonify(tempData)
+
+
+
+@app.route('/api/v1.0/datesearch/<startDate>')
+def start(startDate):
+    sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    results =  (session.query(*sel)
+                       .filter(func.strftime("%Y-%m-%d", Measurement.date) >= startDate)
+                       .group_by(Measurement.date)
+                       .all())
+
+    dates = []                       
+    for result in results:
+        date_dict = {}
+        date_dict["Date"] = result[0]
+        date_dict["Low Temp"] = result[1]
+        date_dict["Avg Temp"] = result[2]
+        date_dict["High Temp"] = result[3]
+        dates.append(date_dict)
+    return jsonify(dates)
+
+
+@app.route('/api/v1.0/datesearch/<startDate>/<endDate>')
+def startEnd(startDate, endDate):
+    sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    results =  (session.query(*sel)
+                       .filter(func.strftime("%Y-%m-%d", Measurement.date) >= startDate)
+                       .filter(func.strftime("%Y-%m-%d", Measurement.date) <= endDate)
+                       .group_by(Measurement.date)
+                       .all())
+
+    dates = []                       
+    for result in results:
+        date_dict = {}
+        date_dict["Date"] = result[0]
+        date_dict["Low Temp"] = result[1]
+        date_dict["Avg Temp"] = result[2]
+        date_dict["High Temp"] = result[3]
+        dates.append(date_dict)
+    return jsonify(dates)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
